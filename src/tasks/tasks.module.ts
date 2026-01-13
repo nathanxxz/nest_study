787483487164ca1cabb4ffe193b9b03ed0d31ba7
Fabricio Nathan <fabricionathan749@gmail.com>
2026-01-12 23:19:57 -0300

    criacao de filter

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { PrismaService } from 'src/database/prisma.service';
+import { APP_FILTER } from '@nestjs/core';
+import { ApiExceptionFilter } from 'src/common/filters/exception-filter';
 
 
 
 @Module({
   imports: [],
   controllers: [TasksController],
-  providers: [TasksService, PrismaService ]
+  providers: [TasksService, PrismaService,
+    {
+      provide: APP_FILTER,
+      useClass: ApiExceptionFilter
+    }
+   ]
 })
 export class TasksModule {
 
